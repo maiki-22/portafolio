c4e3c87d362e65fd236bdea79d31f155da36cada
maiki-22 <miguel.cortes.sandoval23@gmail.com>
2024-12-06 00:41:38 -0300

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maiki-dev.me/',
-  base: '/',
+  base: '/portafolio/',
   integrations: [tailwind(),react()]
 });
